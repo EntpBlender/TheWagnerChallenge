9228f75a61287e9c78d39c914948935a6541aefa
Sohil Ellabban <88499763+EntpBlender@users.noreply.github.com>
2023-06-30 11:13:39 +0000

    First commit

---
+++ Script.py
@@ -0,0 +1,40 @@
+def script():
+
+    Num = int(input("Enter the number of different cards: "))
+    insertnum = 0
+    Array = []
+    for x in range(2*Num):
+        y = x+1
+        print(y)
+        #insertnum = insertnum + 1
+        #Array.append(insertnum)
+
+
+
+
+
+
+# CHECKING MAIN
+if __name__ == "__main__":
+    script()
+else:
+    print("Wrong file")
+
+
+
+
+
+#def CardListMaker(Num):
+   # cardlist = []
+    #for x in range(Num):
+       # match x:
+         #   case 0:
+        #        cardlist.append("A")
+       #     case 1:
+      #          cardlist.append("B")
+     #       case 2:
+    #            cardlist.append("C")
+   #         case 3:
+  #              cardlist.append("D")
+ #           case 4:
+#                cardlist.append("E")
